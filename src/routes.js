762e4b9a9d36c1b4729648d16d51b03de52f9827
Miguel-Leite <miguelleite200leite@gmail.com>
2022-09-26 18:24:46 +0100

    create controller

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const QuestionController = require('./controllers/QuestionController')
 
 const routes = express.Router()
 
 routes.get('/', (req, res) => res.render('index'))
 routes.get('/room', (req, res) => res.render('room'))
 routes.get('/create-pass', (req, res) => res.render('create-pass'))
+
+// Set controllers
+routes.post('/room/:room/:question/:action', QuestionController.index)
 
 module.exports = routes
