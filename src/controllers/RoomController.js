ca73c41696a8a0144f0c6a21ea1187875faba4d1
Miguel-Leite <miguelleite200leite@gmail.com>
2022-09-27 20:25:29 +0100

    create room

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -4,16 +4,26 @@ module.exports = {
         const db = await Database()
         const pwd = req.body.password;
         let roomId = '';
-        for (let index = 0; index < 6; index++) {
-            roomId += Math.floor(Math.random() * 10).toString()
-        }
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-            ) VALUES (
-                ${parseInt(roomId)},
-                ${pwd}
+        let isRoom = true;
+        while (isRoom) {
+
+            for (let index = 0; index < 6; index++) {
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+            isRoom = roomsExistIds.some(id => id === parseInt(roomId))
+
+            if (!isRoom) {
+                await db.run(`INSERT INTO rooms (
+                id,
+                pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pwd}
                 )`);
+            }
+        }
         await db.close()
         res.redirect(`/room/${roomId}`)
     }
