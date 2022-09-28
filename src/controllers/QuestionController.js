a528744af4d88631949c858e05c3ec5bcc48c9e2
Miguel-Leite <miguelleite200leite@gmail.com>
2022-09-28 10:35:31 +0100

    create questions

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,3 +1,4 @@
+const Database = require('../db/config')
 module.exports = {
     index(req, res) {
         const roomId = req.params.room;
@@ -6,5 +7,16 @@ module.exports = {
         const pwd = req.body.password;
 
         console.log(`room = ${roomId}, questionId = ${questionId}, action = ${action}, password = ${pwd}`)
+    },
+    async create(req, res) {
+        const db = await Database()
+        const question = req.body.question;
+        const roomId = req.params.room
+
+        await db.run(`INSERT INTO questions (title, read, room_id) VALUES ("${question}",0,${roomId})`)
+
+        await db.close()
+
+        res.redirect(`/room/${roomId}`);
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,16 @@ const routes = express.Router()
 
 routes.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
 routes.get('/create-pass', (req, res) => res.render('index', { page: 'create-room' }))
-routes.get('/room/:room', (req, res) => res.render('room'))
+    // routes.get('/room/:room', (req, res) => res.render('room'))
+routes.get('/room/:room', RoomController.open)
 
 // Set controllers with routers type POST
-routes.post('/room/:room/:question/:action', QuestionController.index)
+
+// room
 routes.post('/create-room', RoomController.create)
+
+// question
+routes.post('/question/create/:room', QuestionController.create)
+routes.post('/room/:room/:question/:action', QuestionController.index)
 
 module.exports = routes
